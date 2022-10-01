60eac701ef50ad02073a93432dc026316be6a7a1
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-10-01 22:23:23 +0300

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('form');
+const inputEmail = document.querySelector('input');
+const inputMessage = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInput() {
+  const dataOutput = {
+    email: inputEmail.value,
+    message: inputMessage.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataOutput));
+}
+if (localStorage.getItem('feedback-form-state') !== null) {
+  const localData = localStorage.getItem('feedback-form-state');
+  const localDataJSON = JSON.parse(localData);
+  inputEmail.value = localDataJSON.email;
+  inputMessage.value = localDataJSON.message;
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const localData = localStorage.getItem('feedback-form-state');
+  const localDataJSON = JSON.parse(localData);
+
+  inputEmail.value = ' ';
+  inputMessage.value = ' ';
+  console.log(localDataJSON);
+  localStorage.clear();
+}
